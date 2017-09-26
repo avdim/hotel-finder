7f093b097468a1fa5e48af1568f484af56692457
dim <avdim@mail.ru>
2017-09-26 10:08:50 +0300

    instrumentation tests

---
+++ android/src/androidTest/java/tutu/ru/hotelfinder/UiAutomationgTest.java
@@ -0,0 +1,67 @@
+package tutu.ru.hotelfinder;
+
+import android.content.ClipData;
+import android.content.ClipboardManager;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Looper;
+import android.support.test.InstrumentationRegistry;
+import android.support.test.runner.AndroidJUnit4;
+import android.support.test.uiautomator.By;
+import android.support.test.uiautomator.UiDevice;
+import android.support.test.uiautomator.UiSelector;
+import android.support.test.uiautomator.Until;
+import android.view.KeyEvent;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+
+import static org.junit.Assert.*;
+
+@RunWith(AndroidJUnit4.class)
+public class UiAutomationgTest {
+private String packageName;
+@Test
+public void useAppContext() throws Exception {
+//	Context appContext = InstrumentationRegistry.getTargetContext();
+//	assertEquals("tutu.ru.hotelfinder", appContext.getPackageName());
+
+	final UiDevice mDevice = UiDevice.getInstance(InstrumentationRegistry.getInstrumentation());
+	mDevice.pressHome();
+
+	final String launcherPackage = mDevice.getLauncherPackageName();
+	assertNotNull(launcherPackage);
+	mDevice.wait(Until.hasObject(By.pkg(launcherPackage).depth(0)), 10_000);
+	
+	final Context context = InstrumentationRegistry.getContext();
+	packageName = "tutu.ru.hotelfinder";
+	final Intent intent = context.getPackageManager().getLaunchIntentForPackage(packageName);
+	intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK);
+	context.startActivity(intent);
+
+	mDevice.wait(Until.hasObject(By.pkg(packageName).depth(0)), 10_000);
+	sleep(4000);
+	mDevice.findObject(new UiSelector().resourceId("tutu.ru.hotelfinder:id/search_button")).click();
+	sleep(2000);
+	Looper.prepare();
+	final ClipboardManager clipboard = (ClipboardManager) InstrumentationRegistry.getTargetContext().getSystemService(Context.CLIPBOARD_SERVICE);
+	clipboard.setPrimaryClip(ClipData.newHtmlText("simple text", "Питер", "Питер"));
+	mDevice.pressKeyCode(KeyEvent.KEYCODE_V, KeyEvent.META_CTRL_ON);
+	sleep(3000);
+	mDevice.pressEnter();
+	sleep(8000);
+	mDevice.findObject(new UiSelector().resourceId("tutu.ru.hotelfinder:id/list_hotel_title")).click();
+	sleep(7000);
+	mDevice.pressBack();
+	sleep(3000);
+}
+
+private void sleep(int timeMs) {
+	try {
+		Thread.sleep(timeMs);
+	} catch(InterruptedException e) {
+		e.printStackTrace();
+	}
+}
+}
